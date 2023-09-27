94caf0159dc901d87ab7e1a473becf4e60424063
Victor <vkp@jysk.com>
2023-09-27 08:39:37 +0200

    New svg images

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
-import Graphql from "../assets/graphql.png";
-import HTML from "../assets/html5.png";
-import NextJS from "../assets/nextjs.png";
-import React from "../assets/react.png";
-import Tailwind from "../assets/tailwind.png";
-import Typescript from "../assets/typescript.png";
+import CSS from "../assets/CSS.svg";
+import Graphql from "../assets/Graphql.svg";
+import HTML from "../assets/Html.svg";
+import Javascript from "../assets/Javascript.svg";
+import NextJS from "../assets/Nextjs.svg";
+import Node from "../assets/Node.svg";
+import React from "../assets/React.svg";
+import Tailwind from "../assets/Tailwind.svg";
+import Typescript from "../assets/Typescript.svg";
 import Divider from "./Divider";
 
 export default function Skills() {
@@ -13,12 +16,15 @@ export default function Skills() {
       <Divider title="Skills" reverse />
       <div className="flex justify-between gap-12">
         <div className="text-lg flex gap-4 mt-8">
-          <Image className="w-28 h-28" src={Typescript} alt="Typescript" />
           <Image className="w-28 h-28" src={React} alt="React" />
-          <Image className="w-28 h-28" src={HTML} alt="HTML" />
           <Image className="w-28 h-28" src={NextJS} alt="NextJS" />
           <Image className="w-28 h-28" src={Tailwind} alt="Tailwind" />
           <Image className="w-28 h-28" src={Graphql} alt="Graphql" />
+          <Image className="w-28 h-28" src={Javascript} alt="Javascript" />
+          <Image className="w-28 h-28" src={Typescript} alt="Typescript" />
+          <Image className="w-28 h-28" src={HTML} alt="HTML" />
+          <Image className="w-28 h-28" src={CSS} alt="CSS" />
+          <Image className="w-28 h-28" src={Node} alt="Node" />
         </div>
       </div>
       {/* <Image
